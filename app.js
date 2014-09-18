8386b29b2f7d7b105d979c4b764973442f0629ba
Xue Fei <fayexuexue@Xues-MacBook-Pro.local>
2014-09-18 16:07:54 -0400

    check in app logic

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 var express = require('express');
 var app = express();
+var marklogic = require("marklogic")
+var conn = require("./env.js").connection
+var db = marklogic.createDatabaseClient(conn)
+var fs = require("fs")
 
 
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname);
 app.set('view engine', 'html');
 
+app.locals.barChartHelper = require('./public/bar_chart_helper');
+var db = marklogic.createDatabaseClient(conn)
+var q = marklogic.queryBuilder
 
-var fixtureData = require('./fixture_data.json');
-app.locals.barChartHelper = require('./bar_chart_helper');
-
+app.use('/public', express.static(__dirname+'/public'));
+app.get('/data', function(req, res) {
+  var name = req.param('name');
+  var query = db.query(q.where(q.collection("temporal")));
+  query.result(function(r){
+  	console.log(r);
+  	return r;
+  }).
+  then(function(r){
+  	console.log("redering....");
+  	res.json(r);
+  });
+});
 
 app.get('/', function(req, res) {
-  res.render('index', { fixtureData: fixtureData });
+  res.sendfile(__dirname + '/index.html');
 });
 
 
