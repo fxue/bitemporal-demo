809ab759fd8e048069d78f9d280db2c1a4cbd538
Dave Cassel <dmcassel@gmail.com>
2015-07-02 14:37:16 -0700

    #4 set up pass-through to REST API

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+var request = require('request');
+
 /* express set up */
 var express = require('express');
 var app = express();
@@ -11,6 +13,33 @@ var marklogic = require('marklogic');
 var conn = require('./env.js').connection;
 var db = marklogic.createDatabaseClient(conn);
 var q = marklogic.queryBuilder;
+
+function proxy(req, res) {
+  var queryString = req.originalUrl.split('?')[1];
+  console.log(req.method + ' ' + req.path + ' proxied to ' + conn.host + ':' + conn.port + req.path + (queryString ? '?' + queryString : ''));
+  var mlReq = request({
+    uri: 'http://' + conn.host + ':' + conn.port + req.originalUrl,
+    method: req.method,
+    path: req.path + (queryString ? '?' + queryString : ''),
+    headers: req.headers,
+    auth: {
+      user: conn.user,
+      password: conn.password,
+      sendImmediately: false
+    }
+  }, function(error, message, response) {
+    if (message.statusCode >= 300) {
+      console.log('Error!\n' + response);
+    }
+  }).on('data', function(chunk) {
+    res.write(chunk);
+  });
+
+  if (req.body !== undefined) {
+    mlReq.write(JSON.stringify(req.body));
+    mlReq.end();
+  }
+}
 
 function getData(collection, res) {
   // query database for documents to be shown
@@ -35,12 +64,22 @@ function getData(collection, res) {
 
 /* use Express as http server */
 app.use('/public', express.static(__dirname+'/public'));
+
 app.get('/data', function(req, res) {
   getData('addr.json', res);
 });
 app.get('/data/:collection', function(req, res) {
   getData(req.param('collection'), res);
 });
+
+app.all('/v1*', function(req, res){
+  proxy(req, res);
+});
+
+app.all('/manage/*', function(req, res){
+  proxy(req, res);
+});
+
 app.get('/delete', function(req, res) {
   // delete temporal document
   // TODO add your code here...
