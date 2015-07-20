9b054126a32e588a8dc88a7d1be02573729b98b8
Kevin Costello <kevincostello213@gmail.com>
2015-07-20 15:01:54 -0700

    Some functionality with sysTime boxes when edit button clicked

--- public/bitemp_graph_helper.js
+++ public/bitemp_graph_helper.js
@@ -33,6 +33,20 @@ function fillText(data, isEditing) {
 
 }
 
+function cancel(chart) {
+  clearTextArea();
+  $('#editButton').show();
+  $('#viewButton').show();
+  $('#deleteButton').show();
+  $('#cancelButton').hide();
+  $('#contents').hide();
+  $('#saveButton').hide();
+  chart.setEditing(false);
+  chart.setViewing(false);
+  chart.setCurrentURI(undefined);
+  $('#sysTimeDiv').addClass('hideSysTimeBoxes');
+}
+
 function save(chart) {
   data = document.getElementById('contents').value.replace(/\n/g, '');
   data = jQuery.parseJSON(data);
@@ -78,22 +92,10 @@ function setupTextArea(uri, isEditing) {
 
 }
 
-function cancel(chart) {
-  clearTextArea();
-  $('#editButton').show();
-  $('#viewButton').show();
-  $('#deleteButton').show();
-  $('#cancelButton').hide();
-  $('#contents').hide();
-  $('#saveButton').hide();
-  chart.setEditing(false);
-  chart.setViewing(false);
-  chart.setCurrentURI(undefined);
-}
-
 function view(uri) {
   if (uri) {
     setupTextArea(uri, false); //false so function knows the document is not being edited
+    $('#sysTimeDiv').addClass('hideSysTimeBoxes');
   }
   else {
     alert('Please click a doc first');
@@ -104,6 +106,7 @@ function edit(uri) {
   console.log('Editing ' + uri);
   if (uri) {
     setupTextArea(uri, true); //true so function knows the document is being edited
+    $('#sysTimeDiv').removeClass('hideSysTimeBoxes');
   }
   else {
     alert('Please click a doc first');
@@ -111,14 +114,13 @@ function edit(uri) {
 }
 
 function deleteDoc(uri) {
-  var origUri = uri;
   if (!uri) { // Not given a valid document uri
     uri = 'addr.json';
   }
   else {
     var lastPeriodLoc = uri.lastIndexOf('.');
     var firstPeriodLoc = uri.indexOf('.');
-    if (lastPeriodLoc !== firstPeriodLoc) { //More than '.', indicates a big number within uri.
+    if (lastPeriodLoc !== firstPeriodLoc) { //More than one '.', indicates a big number within uri.
       uri = uri.substring(0, firstPeriodLoc) + uri.substring(lastPeriodLoc, uri.length); // Remove the big number.
     }
   }
@@ -154,7 +156,21 @@ var removeButtonEvents = function () {
   $('#viewButton').unbind('click');
   $('#saveButton').unbind('click');
   $('#change-prop').unbind('click');
-}
+};
+
+var drawChart = function (params, docProp) {
+  var chart = barChart()
+    .data(params.data)
+    .width(params.width)
+    .height(params.height)
+    .setDisplayProperty(docProp);
+
+  var selector = '#' + params.containerId;
+  d3.select(selector + ' .chart').remove();
+  d3.select(selector).append('div').classed('chart', true).call(chart);
+
+  return chart;
+};
 
 var getBarChart = function (params, docProp) {
   var chart = drawChart(params, docProp);
@@ -183,19 +199,5 @@ var getBarChart = function (params, docProp) {
   $('#change-prop').click(function() {
     changeTextInGraph(chart, params);
   });
-}
-
-var drawChart = function (params, docProp) {
-  var chart = barChart()
-    .data(params.data)
-    .width(params.width)
-    .height(params.height)
-    .setDisplayProperty(docProp);
-
-  var selector = '#' + params.containerId;
-  d3.select(selector + ' .chart').remove();
-  var chartDiv = d3.select(selector).append('div').classed('chart', true).call(chart);
-
-  return chart;
-}
+};
 
