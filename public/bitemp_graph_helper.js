1a61acd20a8f1fa2dc1fe574c11749b7ca278649
Kevin Costello <kevincostello213@gmail.com>
2015-07-20 13:15:58 -0700

    Rebasing with Ash+Hil code, trying to get sysTime boxes to toggle visibility(not working)

--- public/bitemp_graph_helper.js
+++ public/bitemp_graph_helper.js
@@ -108,8 +108,6 @@ function cancel(chart) {
   $('#cancelButton').hide();
   $('#contents').hide();
   $('#saveButton').hide();
-  $('#sysStartVal').hide();
-  $('#sysEndVal').hide();
   chart.setEditing(false);
   chart.setViewing(false);
   chart.setCurrentURI(undefined);
