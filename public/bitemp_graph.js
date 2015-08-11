0d009dc8e090cb7e95ec5bb656c88a1166163ca1
Kevin Costello <kevincostello213@gmail.com>
2015-08-11 13:39:37 -0700

    Some jshint fixes, and fixed an error where the view/edit/delete button disappeared upon a successful delete

--- public/bitemp_graph.js
+++ public/bitemp_graph.js
@@ -1,4 +1,4 @@
-/*global d3, moment */
+/*global d3, moment*/
 
 function showCurrURI(uri) {
   document.getElementById('selectedURI').innerHTML = 'Selected URI: ' + uri.bold();
@@ -592,14 +592,14 @@ var barChart = function() {
           var dx = xScale(moment(date).toDate());
           if (textId.includes('start')) {
             if (date > new Date(xMax).toISOString() || date < new Date(xMin).toISOString()) {
-              alert('This time is out of the axis time range');
-              dx = xScale(moment(xMin).toDate())
+              window.alert('This time is out of the axis time range');
+              dx = xScale(moment(xMin).toDate());
               $('#'+textId).val(format(xMin));
             }
           }
           if (textId.includes('end')) {
             if (date > new Date(xMax).toISOString() || date < new Date(xMin).toISOString()) {
-              alert('This time is out of the axis time range');
+              window.alert('This time is out of the axis time range');
               $('#'+textId).val(format(xMax));
               dx = xScale(moment(xMax).toDate());
             }
@@ -611,16 +611,16 @@ var barChart = function() {
           var dy = yScale(moment(date).toDate());
           if (textId.includes('start')) {
             if (date > new Date(yMax).toISOString() || date < new Date(yMin).toISOString()) {
-              alert('This time is out of the axis time range');
+              window.alert('This time is out of the axis time range');
               $('#'+textId).val(format(yMin));
-              dy = yScale(moment(yMin).toDate())
+              dy = yScale(moment(yMin).toDate());
             }
             dy = -(height-margin.top-margin.bottom-dy);
           }
 
           else {
             if (date > new Date(yMax).toISOString() || date < new Date(yMin).toISOString()) {
-              alert('This time is out of the axis time range');
+              window.alert('This time is out of the axis time range');
               $('#'+textId).val(format(yMax));
               dy = yScale(moment(yMax).toDate());
             }
