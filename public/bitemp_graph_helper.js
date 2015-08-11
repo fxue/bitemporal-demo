7ecf7ed0d69db23b96382894afc36963f3235fbd
lrhruska <lrhruska@calpoly.edu>
2015-08-11 13:34:58 -0700

    fixed error sorry everyone!

--- public/bitemp_graph_helper.js
+++ public/bitemp_graph_helper.js
@@ -129,41 +129,43 @@ function fillText(data, isEditing, id) {
   clearTextArea();
   var textArea = document.getElementById(id);
 
-  if(data.contentType.indexOf('xml') > -1) {//view xml doc
-    var xmlStr = data.childNodes[0].outerHTML;
-    //https://gist.github.com/sente/1083506
-    //to format pretty printing of xml
-    function formatXml(xml) {
-      var formatted = '';
-      var reg = /(>)(<)(\/*)/g;
-      xml = xml.replace(reg, '$1\r\n$2$3');
-      var pad = 0;
-      jQuery.each(xml.split('\r\n'), function(index, node) {
-        var indent = 0;
-        if (node.match( /.+<\/\w[^>]*>$/ )) {
-          indent = 0;
-        } else if (node.match( /^<\/\w/ )) {
-          if (pad != 0) {
-            pad -= 1;
+  if(data.contentType) {
+    if(data.contentType.indexOf('xml') > -1) {//view xml doc
+      var xmlStr = data.childNodes[0].outerHTML;
+      //https://gist.github.com/sente/1083506
+      //to format pretty printing of xml
+      function formatXml(xml) {
+        var formatted = '';
+        var reg = /(>)(<)(\/*)/g;
+        xml = xml.replace(reg, '$1\r\n$2$3');
+        var pad = 0;
+        jQuery.each(xml.split('\r\n'), function(index, node) {
+          var indent = 0;
+          if (node.match( /.+<\/\w[^>]*>$/ )) {
+            indent = 0;
+          } else if (node.match( /^<\/\w/ )) {
+            if (pad != 0) {
+              pad -= 1;
+            }
+          } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+            indent = 1;
+          } else {
+            indent = 0;
           }
-        } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
-          indent = 1;
-        } else {
-          indent = 0;
-        }
-        var padding = '';
-        for (var i = 0; i < pad; i++) {
-          padding += '  ';
-        }
-        formatted += padding + node + '\r\n';
-        pad += indent;
-      });
+          var padding = '';
+          for (var i = 0; i < pad; i++) {
+            padding += '  ';
+          }
+          formatted += padding + node + '\r\n';
+          pad += indent;
+        });
 
-      return formatted;
+        return formatted;
+      }
+      textArea.value = formatXml(xmlStr);
     }
-    textArea.value = formatXml(xmlStr);
   }
-  
+
   else {//view json doc
     textArea.value += '{';
     var strToAdd;
