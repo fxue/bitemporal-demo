59e5a0e0da61e6bec46a6aea774851bc2e89f867
Ashley Dattalo <adattalo@calpoly.edu>
2015-08-14 10:30:42 -0700

    deleted extra spaces

--- public/search.js
+++ public/search.js
@@ -1,6 +1,6 @@
 /* global parseData */
 var firstDoc, lastDoc;
- 
+
 function generateOps() {
   var operators = ['None', 'ALN_EQUALS', 'ALN_CONTAINS', 'ALN_CONTAINED_BY', 'ALN_MEETS', 'ALN_MET_BY', 'ALN_BEFORE', 'ALN_AFTER', 'ALN_STARTS', 'ALN_STARTED_BY', 'ALN_FINISHES', 'ALN_FINISHED_BY', 'ALN_OVERLAPS', 'ALN_OVERLAPPED_BY', 'ISO_OVERLAPS', 'ISO CONTAINS', 'ISO_PRECEDES', 'ISO_SUCCEEDS', 'ISO_IMM_PRECEDES', 'ISO_IMM_SUCCEEDS', 'ISO_EQUALS'];
   for( var i= 0; i < operators.length; i++ ) {
@@ -8,7 +8,7 @@ function generateOps() {
     $('#sysDropdown').append($('<option>').text(operators[i]));
   }
 }
- 
+
 function getSelected(id) {
   var dropDownList = document.getElementById(id);
   return dropDownList.options[dropDownList.selectedIndex].value;
@@ -53,18 +53,18 @@ $('#resetButton').click(function() {
   document.getElementById('dragInstruct').innerHTML = '*Select an operator and drag the red bars to create your selected time range*';
   $('#resetButton, .sysTimesDisplay, .valTimesDisplay, #errorMessage').css({'visibility': 'hidden'});
 });
- 
+
 function runSearchQuery() {
   var selectedColl = getSelected('dropdown');
   var valSelectedOp = getSelected('valDropdown');
   var sysSelectedOp = getSelected('sysDropdown');
- 
+
   var valAxis = '';
   var sysAxis = '';
 
   var valStart = '';
   var valEnd = '';
- 
+
   if(valSelectedOp !== 'None') {
     valAxis = 'myValid';
     valStart = document.getElementById('startValBox').value;
@@ -76,10 +76,10 @@ function runSearchQuery() {
     valStart = new Date(valStart).toISOString();
     valEnd = new Date(valEnd).toISOString();
   }
- 
+
   var sysStart = '';
   var sysEnd = '';
- 
+
   if(sysSelectedOp !== 'None') {
     sysAxis = 'mySystem';
     sysStart = document.getElementById('startSysBox').value;
@@ -91,12 +91,12 @@ function runSearchQuery() {
     sysStart = new Date(sysStart).toISOString();
     sysEnd = new Date(sysEnd).toISOString();
   }
- 
+
   if(valSelectedOp === 'None' && sysSelectedOp === 'None' ) {
     ajaxTimesCall(selectedColl, null, false);
     return;
   }
- 
+
   $.ajax({
       url: '/v1/resources/operators?rs:collection='+selectedColl+'&rs:valAxis='+valAxis+'&rs:valSelectedOp='+valSelectedOp+'&rs:sysAxis='+sysAxis+'&rs:sysSelectedOp='+sysSelectedOp+'&rs:valStart='+valStart+'&rs:valEnd='+valEnd+'&rs:sysStart='+sysStart+'&rs:sysEnd='+sysEnd,
       success: function(response, textStatus)
@@ -111,11 +111,11 @@ function runSearchQuery() {
   });
 
 }
- 
+
 function displayQuery(response) {
   document.getElementById('queryText').innerHTML = response.query;
 }
- 
+
 function formatData(response) {
   var result = [];
   if(response.values) {
@@ -125,7 +125,7 @@ function formatData(response) {
   }
   return result;
 }
- 
+
 $('#dropdown').change(function() {
   $('#next, #prev, .hide, #startValBox, #endValBox, #startSysBox, #endSysBox').css({'visibility': 'hidden'});
   var selectedColl = getSelected('dropdown');
@@ -136,7 +136,7 @@ $('#dropdown').change(function() {
   document.getElementById('valDropdown').selectedIndex = 0;
   document.getElementById('sysDropdown').selectedIndex = 0;
 });
- 
+
 //function to make ajax call to get min and max times
 function ajaxTimesCall(selectedColl, dataToDisplay, visibleBars) {
   $.ajax(
@@ -153,7 +153,7 @@ function ajaxTimesCall(selectedColl, dataToDisplay, visibleBars) {
           }
           drag = false;
         }
- 
+
         var times = response;
         var timeRanges = {
           valStart: toReturnDate(times.valStart),
@@ -161,7 +161,7 @@ function ajaxTimesCall(selectedColl, dataToDisplay, visibleBars) {
           sysStart: toReturnDate(times.sysStart),
           sysEnd: toReturnDate(times.sysEnd)
         };
- 
+
         if(!drag) {
           document.getElementById('vertBar1').innerHTML = 'Start Time:' + '&nbsp;&nbsp;' + $('#startSysBox').val().bold();
           document.getElementById('vertBar2').innerHTML = 'End Time:' + '&nbsp;&nbsp;&nbsp;' + $('#endSysBox').val().bold();
@@ -174,7 +174,7 @@ function ajaxTimesCall(selectedColl, dataToDisplay, visibleBars) {
           document.getElementById('valDropdown').disabled=true;
           document.getElementById('sysDropdown').disabled=true;
         }
- 
+
         getBarChart({
           data: data,
           width: 800,
@@ -183,9 +183,9 @@ function ajaxTimesCall(selectedColl, dataToDisplay, visibleBars) {
           yAxisLabel: 'Valid',
           timeRanges: timeRanges,
           draggableBars: drag,
-         containerId: 'bar-chart-large'
+          containerId: 'bar-chart-large'
         }, null);
-        
+
         if (visibleBars) {
           if (getSelected('sysDropdown') !== 'None') {
             $('#dragLeft').css({'visibility': 'visible'});
@@ -219,7 +219,7 @@ function toReturnDate(time) {
     return null;
   }
 }
- 
+
 //function when search button is clicked
 $('#search').click(function() {
   firstDoc = 1;
@@ -227,21 +227,21 @@ $('#search').click(function() {
   displayDocs(firstDoc, lastDoc);
   $('#next, #prev').css({'visibility': 'visible'});
 });
- 
+
 //function when the next button is clicked
 $('#next').click(function() {
   firstDoc+=10;
   lastDoc+=10;
   displayDocs(firstDoc, lastDoc);
 });
- 
+
 //function when the prev button is clicked
 $('#prev').click(function() {
   firstDoc-=10;
   lastDoc-=10;
   displayDocs(firstDoc, lastDoc);
 });
- 
+
 /**
 * Display docs is a function that displays the physical and logcial documents
 * corresponding to the collection selected in the dropdown box.
@@ -269,7 +269,7 @@ function displayDocs(start, end) {
       console.log('problem');
     }
   });
- 
+
   function onDisplayDocs(data, textStatus, response) {
     var docs;
     var totalDocLen = response.getResponseHeader('vnd.marklogic.result-estimate');
@@ -286,7 +286,7 @@ function displayDocs(start, end) {
     // Looks at the index of the first and last document (passed into the function)
     // and disables or enables the next/previous buttons based on those indexes.
     document.getElementById('prev').disabled = start <= 1;
- 
+
     if (end >= totalDocLen) {
      document.getElementById('next').disabled = true;
       end = totalDocLen;
@@ -294,14 +294,14 @@ function displayDocs(start, end) {
     else {
       document.getElementById('next').disabled = false;
     }
- 
+
     if (parseInt(totalDocLen) === 0) {
       document.getElementById('numDocs').innerHTML = start - 1 + ' to ' + end + ' of ' + totalDocLen;
     }
     else {
       document.getElementById('numDocs').innerHTML = start + ' to ' + end + ' of ' + totalDocLen;
     }
- 
+
     //Loops through the documents to get the URI and the valid and system times
     //Calls functions to display the information on the search page
     //Checks if docs has a defined value
@@ -315,12 +315,12 @@ function displayDocs(start, end) {
           uriLogical = collArr[t];
         }
       }
- 
+
       var sysStart = docs[i].content.sysStart;
       var sysEnd = docs[i].content.sysEnd;
       var validStart = docs[i].content.valStart;
       var validEnd = docs[i].content.valEnd;
- 
+
       bullet
         .append($('<hr id=\'break\'>')
         )
@@ -371,13 +371,13 @@ function buildDate( startDate, endDate, label ) {
  
   return date;
 }
- 
+
 /**
-* Shortens the date to only include the day, month, year, and time.
-* The time appears as hours, minutes, seconds, excluding 'GMT'
-*
-* @param date: the date to shorten
-*/
+ * Shortens the date to only include the day, month, year, and time.
+ * The time appears as hours, minutes, seconds, excluding 'GMT'
+ *
+ * @param date: the date to shorten
+ */
 function shortenDate( date ) {
   date = date.toString().split(' ');
   if (date[3] >= '9999') {
